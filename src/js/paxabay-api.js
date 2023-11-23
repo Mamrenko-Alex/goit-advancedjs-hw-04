ee2fde46e4fe652b661a47bec36527811709c0c7
Mamrenko-Alex <Mamrenkodev@yandex.com>
2023-11-23 22:37:58 +0100

    add method get

--- src/js/paxabay-api.js
+++ src/js/paxabay-api.js
@@ -1,11 +1,10 @@
 import axios from "axios";
+export { getImages };
+    
+const KEY = '40858568-c2cbea82fefa532e5a07e25a9'
 
-axios.defaults.headers['key'] = '40858568-c2cbea82fefa532e5a07e25a9';
-axios.defaults.headers['q'] = 'yellow+flowers';
-axios.defaults.headers['image_type'] = 'photo';
-
-
-export function getImages(params) {
-    return axios.get('https://pixabay.com/api/')
-        .then(resp => resp.json())
+async function getImages(search) {
+    const data = await axios.get(`https://pixabay.com/api/?key=${KEY}&q=${search}&image_type=photo&orientation=horizontal&safesearch=true`)
+        .then(res => res.data)
+    return data
 }
--- src/main.js
+++ src/main.js
@@ -1,4 +1,28 @@
 import { getImages } from "./js/paxabay-api";
+import iziToast from "izitoast";
+import 'izitoast/dist/css/iziToast.min.css'
 
-getImages()
-    .then(data => console.log(data))
+const searchForm = document.querySelector('.search-form');
+const searchField = searchForm.elements.searchQuery;
+
+searchForm.addEventListener('submit', submitHandler)
+
+
+function submitHandler(event) {
+    event.preventDefault()
+
+    getImages(searchField.value)
+        .then(data => {
+            const images = data.hits
+                .map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+                    return {webformatURL, largeImageURL, tags, likes, views, comments, downloads}
+                })
+            console.log(images);
+        }).catch(
+            iziToast.error({
+                title: 'Error',
+                message: 'Sorry, there are no images matching your search query. Please try again.',
+            }))
+
+    searchField.value = ''
+}
